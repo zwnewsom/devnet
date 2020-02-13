2a25713561783e0eb5d1a27ff107cb683cff8f82
Zach Newsom <zwnewsom@gmail.com>
2020-02-12 20:51:06 -0500

    added mongoose logic to connect to db

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'));
 
-const PORT = process.env.PORT || 5000;
+// connect database
+connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
