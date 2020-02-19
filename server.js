997de9df4b0f4845d6db77ce084b2bb7386274b8
Zach Newsom <zwnewsom@gmail.com>
2020-02-19 15:30:30 -0500

    added models directory and mongoose schema for Users

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+// Connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
 
-// connect database
-connectDB();
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
