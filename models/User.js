75761d6faad56e4715f8ba8e0080ebc808955730
Zach Newsom <zwnewsom@gmail.com>
2020-02-19 19:17:30 -0500

    deleted duplicated routes/api

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new.mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -22,3 +22,5 @@ const UserSchema = new.mongoose.Schema({
     default: Date.now
   }
 });
+
+module.exports = User = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 // Connect database
 connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 
@@ -13,5 +16,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+const PORT = 5000; //process.env.PORT ||
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
